12ba69d837ccd9199a71b7dc4f79013a48ac1fd3
rdeja <78109640+rdeja@users.noreply.github.com>
2022-04-08 08:49:32 +0200

    change

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Account account = new Account(100);
         System.out.println(account.withdraw(30));
+        // next change
         System.out.println("Hello");
     }
 }
